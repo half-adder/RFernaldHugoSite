06948397ad11640452c58de9636c807c5b5b76fa
Sean Johnsen <sean.b.johnsen@gmail.com>
2023-07-18 15:24:59 -0400

    use webcam shader example

--- static/js/example-sketch.js
+++ static/js/example-sketch.js
@@ -1,91 +1,75 @@
+// in this sketch we're going to create a feedback effect by repeatedly sending the same image back to the shader and performing a slight modification
+// click the mouse to get things started
 
-let w;
-let columns;
-let rows;
-let board;
-let next;
+// the shader variable
+let camShader;
+
+// the camera variable
+let cam;
+
+// we will need at least two layers for this effect
+let shaderLayer;
+let copyLayer;
+
+function preload() {
+    // load the shader
+    camShader = loadShader('/shaders/shader.vert', '/shaders/shader.frag');
+}
 
 function setup() {
-    // Set simulation framerate to 10 to avoid flickering
-    frameRate(10);
-    var canvas = createCanvas(720, 400);
+    // disables scaling for retina screens which can create inconsistent scaling between displays
+    pixelDensity(1);
+
+    // shaders require WEBGL mode to work
+    var canvas = createCanvas(400, 400);
     canvas.parent('content-div');
-    w = 20;
-    // Calculate columns and rows
-    columns = floor(width / w);
-    rows = floor(height / w);
-    // Wacky way to make a 2D array is JS
-    board = new Array(columns);
-    for (let i = 0; i < columns; i++) {
-        board[i] = new Array(rows);
-    }
-    // Going to use multiple 2D arrays and swap them
-    next = new Array(columns);
-    for (i = 0; i < columns; i++) {
-        next[i] = new Array(rows);
-    }
-    init();
+    noStroke();
+
+    // initialize the webcam at the window size
+    cam = createCapture(VIDEO);
+    cam.size(400, 400);
+
+    // hide the html element that createCapture adds to the screen
+    cam.hide();
+
+    // this layer will use webgl with our shader
+    shaderLayer = createGraphics(400, 400, WEBGL);
+
+    // this layer will just be a copy of what we just did with the shader
+    copyLayer = createGraphics(400, 400);
+
 }
 
 function draw() {
-    background(255);
-    generate();
-    for (let i = 0; i < columns; i++) {
-        for (let j = 0; j < rows; j++) {
-            if ((board[i][j] == 1)) fill(0);
-            else fill(255);
-            stroke(0);
-            rect(i * w, j * w, w - 1, w - 1);
-        }
-    }
+    // shader() sets the active shader with our shader
 
+    shaderLayer.shader(camShader);
+
+    // lets just send the cam to our shader as a uniform
+    camShader.setUniform('tex0', cam);
+
+    // also send the copy layer to the shader as a uniform
+    camShader.setUniform('tex1', copyLayer);
+
+    // send mouseIsPressed to the shader as a int (either 0 or 1)
+    camShader.setUniform('mouseDown', int(mouseIsPressed));
+    camShader.setUniform("u_mouse", [map(mouseX, 0, width, 0.5, 1.1), map(mouseY, 0, height, height, 0) / height]);
+
+    camShader.setUniform('time', frameCount * 0.01);
+
+    // rect gives us some geometry on the screen
+    shaderLayer.rect(0, 0, width, height);
+
+    // draw the shaderlayer into the copy layer
+    copyLayer.image(shaderLayer, 0, 0, width, height);
+
+    // render the shaderlayer to the screen
+    image(shaderLayer, 0, 0, width, height);
+
+    textSize(24);
+    text("Click.", 50, 50);
 }
 
-// reset board when mouse is pressed
-function mousePressed() {
-    init();
-}
-
-// Fill board randomly
-function init() {
-    for (let i = 0; i < columns; i++) {
-        for (let j = 0; j < rows; j++) {
-            // Lining the edges with 0s
-            if (i == 0 || j == 0 || i == columns - 1 || j == rows - 1) board[i][j] = 0;
-            // Filling the rest randomly
-            else board[i][j] = floor(random(2));
-            next[i][j] = 0;
-        }
-    }
-}
-
-// The process of creating the new generation
-function generate() {
-
-    // Loop through every spot in our 2D array and check spots neighbors
-    for (let x = 1; x < columns - 1; x++) {
-        for (let y = 1; y < rows - 1; y++) {
-            // Add up all the states in a 3x3 surrounding grid
-            let neighbors = 0;
-            for (let i = -1; i <= 1; i++) {
-                for (let j = -1; j <= 1; j++) {
-                    neighbors += board[x + i][y + j];
-                }
-            }
-
-            // A little trick to subtract the current cell's state since
-            // we added it in the above loop
-            neighbors -= board[x][y];
-            // Rules of Life
-            if ((board[x][y] == 1) && (neighbors < 2)) next[x][y] = 0;           // Loneliness
-            else if ((board[x][y] == 1) && (neighbors > 3)) next[x][y] = 0;           // Overpopulation
-            else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1;           // Reproduction
-            else next[x][y] = board[x][y]; // Stasis
-        }
-    }
-
-    // Swap!
-    let temp = board;
-    board = next;
-    next = temp;
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
